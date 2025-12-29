1b32fda1164769138064ab016099012fca5d00b9
mohitrajgarh <mohitsainirajgarh@gmail.com>
2025-12-29 06:25:35 +0530

    feat(footer): add footer with mobile responsive

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer/footer";
 import Navbar from "@/components/navbar/navbar";
 
 export default function SiteLayout({
@@ -9,7 +10,7 @@ export default function SiteLayout({
     <div className="flex min-h-screen flex-col">
       <Navbar />
       <main className="flex-1">{children}</main>
-      {/* Footer can be added here later */}
+      <Footer />
     </div>
   );
 }
