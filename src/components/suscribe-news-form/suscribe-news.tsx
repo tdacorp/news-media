d4d1a6d7676b437f0f6591165d9ca83d3fdb2d92
mohitrajgarh <mohitsainirajgarh@gmail.com>
2026-01-02 12:44:20 +0530

    fix : fix  lint waring remove input

--- src/components/suscribe-news-form/suscribe-news.tsx
+++ src/components/suscribe-news-form/suscribe-news.tsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
-import { Input } from "../ui/input";
-
 
 export default function SubscribeNewsForm() {
     return (
@@ -16,11 +14,6 @@ export default function SubscribeNewsForm() {
                         Get the latest news delivered directly to your inbox
                     </p>
                     <div className="space-y-2">
-                        {/* <Input
-                            type="email"
-                            placeholder="Enter your email"
-                            className="bg-primary-foreground/10 border-primary-foreground/20 text-primary-foreground placeholder:text-primary-foreground/60"
-                        /> */}
                         <Link href={"/contact"}>
                          <Button className="w-full bg-primary-foreground text-primary hover:bg-primary-foreground/90">
                             Subscribe
